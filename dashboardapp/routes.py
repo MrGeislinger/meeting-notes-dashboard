eda0249dae9e4dc793fee758b126bd38ec95df65
MrGeislinger <GitHub@MrGeislinger.com>
2020-03-09 22:28:00 -0700

    feat: Display one-on-one template; need event data

--- dashboardapp/routes.py
+++ dashboardapp/routes.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from dashboardapp import app
 
-from flask import render_template
+from datetime import datetime, timedelta
 
 @app.route('/')
 @app.route('/index')
@@ -10,5 +11,33 @@ def index():
 
 @app.route('/one-on-one')
 @app.route('/1-on-1')
+@app.route('/1-1')
 def project_one():
-    return render_template('one-on-one.html')
+    # TODO: Find relevant meeting (student) by time for apppointment
+    # TODO: Get current event name to display
+    # event_name = event['summary']
+    event_name = 'NAME OF EVENT'
+
+    # Date display format
+    DATE_FORMAT_STRING = '%H:%M - %a %b %d'
+    # TODO: Get time of event (start and end)
+    now = datetime.now()
+    event_time_start = now.strftime(DATE_FORMAT_STRING)
+    event_time_end = (now + timedelta(0,30*60)).strftime(DATE_FORMAT_STRING)
+
+    # TODO: Pull other information relevant for template (from event)
+    student_name = 'STUDENT NAME'
+    zoom_link = 'https://example.com/zoomlink'
+
+    # TODO: Pull info from database
+    prev_status = 'Previous status for student updates'
+
+    return render_template(
+        'one-on-one.html',
+        student_name = student_name,
+        zoom_link = zoom_link,
+        status = prev_status,
+        event_name = event_name,
+        event_time_start = event_time_start,
+        event_time_end = event_time_end
+    )
